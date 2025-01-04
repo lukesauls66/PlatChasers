2070b868586008b6dee15bcbc6be420415e7a12a
Luke Sauls <luke.sauls279@gmail.com>
2025-01-03 17:53:11 -0700

    got rid of practice line

--- components/LoginPage/LogIn/LogIn.tsx
+++ components/LoginPage/LogIn/LogIn.tsx
@@ -5,7 +5,6 @@ import { signIn } from "next-auth/react";
 const LogIn: React.FC = () => {
   return (
     <div className="flex flex-col">
-      <p>choose one</p>
       <button onClick={() => signIn("google")}>Sign in with Google</button>
       <button onClick={() => signIn("discord")}>Sign in with Discord</button>
       <button onClick={() => signIn("facebook")}>Sign in with Facebook</button>
