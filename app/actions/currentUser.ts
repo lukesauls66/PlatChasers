9cf1780f95229a1fc5c1626547c9b81573d4f90d
lukesauls66 <luke.saul279@gmail.com>
2025-01-09 22:31:25 -0700

    currentUser available for endpoints

---
+++ app/actions/currentUser.ts
@@ -0,0 +1,16 @@
+import { auth } from "@/auth";
+
+export default async function GET() {
+  try {
+    const session = await auth();
+
+    if (!session?.user) {
+      return null;
+    }
+
+    return session.user;
+  } catch (error) {
+    console.error("Error getting session: ", error);
+    return null;
+  }
+}
