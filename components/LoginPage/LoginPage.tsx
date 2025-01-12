29919b913c49aaf07a97d329045a28999ab21619
lukesauls66 <luke.saul279@gmail.com>
2025-01-12 10:25:26 -0700

    auth error handling complete

--- components/LoginPage/LoginPage.tsx
+++ components/LoginPage/LoginPage.tsx
@@ -9,26 +9,49 @@ import { FaFacebook } from "react-icons/fa";
 import { FaDiscord } from "react-icons/fa";
 import { Button } from "../ui/button";
 
-export const login = async (email: string, password: string) => {
+export const login = async (
+  email: string,
+  password: string
+): Promise<boolean> => {
   try {
-    await signIn("credentials", {
+    const res = await signIn("credentials", {
       email,
       password,
       redirect: false,
       callbackUrl: "/",
     });
+
+    if (res?.error) {
+      console.log("Login failed: ", res.error);
+      return false;
+    }
+
+    console.log("Login successful: ", res);
+    return true;
   } catch (error) {
     console.log("ERROR: ", error);
+    return false;
   }
 };
 
 const LoginPage: React.FC = () => {
   const { setVariant } = useVariant();
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const [errors, setErrors] = useState<string | null>(null);
+
+  const handleLogin = async () => {
+    const res = await login(email, password);
+
+    if (res) {
+      setVariant("home");
+    } else {
+      setErrors("Invalid credentials");
+    }
+  };
 
   return (
-    <div className="flex flex-col gap-2 items-center">
+    <div className="flex flex-col gap-2 items-center bg-[#e7e7e7] min-h-screen">
       <h1 className="text-2xl py-4 font-semibold">Sign In</h1>
       <div className="flex flex-col gap-6 items-center">
         <div className="flex flex-col items-center gap-5">
@@ -52,35 +75,23 @@ const LoginPage: React.FC = () => {
               }
             />
           </div>
+          {errors && <p className="text-[#ae3634]">{errors}</p>}
           <Button
-            onClick={() => {
-              login(email, password);
-              setVariant("home");
-            }}
+            onClick={handleLogin}
             variant={"destructive"}
             size={"lg"}
-            className="bg-purple-700 hover:bg-purple-700/80"
+            className="bg-[#53285f]/90 hover:bg-purple-700/80"
           >
             Sign In
           </Button>
-          {/* <button
-            onClick={() => {
-              login(email, password);
-              setVariant("home");
-            }}
-            className="rounded-md py-2 w-[9rem] text-md border-[1px] border-black"
-            type="submit"
-          >
-            Sign In
-          </button> */}
         </div>
 
         <div className="flex flex-col items-center gap-1">
           <p className="font-semibold">DISCLAIMER:</p>
           <p className="flex text-center">
-            Please create an account through this site using the email you use
-            with your provider (Google, Discord, or Facebook) before logging in
-            with your provider in order to link said account to our site.
+            Please create an account using the email you use with your provider
+            (Google, Discord, or Facebook) in order to automatically link
+            accounts.
           </p>
           <p>Thank you!</p>
         </div>
@@ -88,7 +99,7 @@ const LoginPage: React.FC = () => {
         <div className="flex flex-col gap-4 items-center">
           <button
             className="flex flex-col-reverse gap-1 items-center text-xl"
-            onClick={() => signIn("google", { redirectTo: "/" })}
+            onClick={() => signIn("google", { callbackUrl: "/" })}
           >
             Sign in with Google <FcGoogle className="w-7 h-7" />
           </button>
--- components/NavBar/NavBar.tsx
+++ components/NavBar/NavBar.tsx
@@ -3,22 +3,23 @@
 import Link from "next/link";
 import { useVariant } from "@/context/Variant";
 import { GiDiamondTrophy } from "react-icons/gi";
+import { GiTrophyCup } from "react-icons/gi";
 import Hamburger from "./Hamburger";
 
 const NavBar: React.FC = () => {
   const { setVariant } = useVariant();
 
   return (
-    <div className="flex items-center justify-between py-5 px-6 bg-purple-600">
+    <div className="flex items-center justify-between py-5 px-6 bg-[#53285f]">
       <Link
         onClick={() => setVariant("home")}
         href="/"
-        className="flex justify-center items-center"
+        className="flex items-center"
       >
-        <h1 className="text-[2rem] md:text-4xl font-bold text-gray-400">
+        <h1 className="text-[2rem] md:text-4xl font-bold text-[#e7e7e7]">
           PlatChasers
         </h1>
-        <GiDiamondTrophy className="w-8 h-[2rem] text-gray-400" />
+        <GiTrophyCup className="w-8 h-[2rem] text-[#e7e7e7] mb-2" />
       </Link>
       <div>
         <Hamburger />
@@ -28,3 +29,5 @@ const NavBar: React.FC = () => {
 };
 
 export default NavBar;
+
+// purple-600
