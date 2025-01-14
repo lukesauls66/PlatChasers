5dd8c342a962b3fcd1f70f36ae791245c590d1f3
lukesauls66 <luke.saul279@gmail.com>
2025-01-13 17:43:31 -0700

    search bar update

--- components/Util/SearchBar.tsx
+++ components/Util/SearchBar.tsx
@@ -57,11 +57,12 @@ const SearchBar: React.FC<SearchBarProps> = ({ games }) => {
         <div className="absolute z-10 w-full mt-1 bg-white border border-gray-300 rounded-md max-h-60 overflow-y-auto shadow-lg">
           {areGamesToSearch ? (
             filteredGames.map((game) => (
-              <div
-                key={game.id}
-                className="px-4 py-2 cursor-pointer hover:bg-gray-100"
-              >
-                <Link href={`/games/${game.id}`}>{game.title}</Link>
+              <div key={game.id}>
+                <Link href={`/games/${game.id}`}>
+                  <div className="px-4 py-2 cursor-pointer hover:bg-gray-100">
+                    <p className="text-center">{game.title}</p>
+                  </div>
+                </Link>
               </div>
             ))
           ) : (
